0e8e02c7fe71d01eeee132b831d49e5c19de209f
Anns Rv <anns.rajarethinam@gmail.com>
2022-07-26 12:51:46 -0400

    trailer v1

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,9 +2,12 @@
 import React, { useEffect, useState } from 'react'
 import client from '../axios';
 import '../styles/Row.css'
+import Youtube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const Row = ({title, fetchUrl, largePic}) => {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setUrl] = useState('')
     const BASE_URL = "https://image.tmdb.org/t/p/original/"
    
     useEffect(() =>{
@@ -16,8 +19,30 @@ const Row = ({title, fetchUrl, largePic}) => {
         
         fetch();
     
-    },[fetchUrl]) 
-
+    },[fetchUrl])
+    
+    // From 'react-youtube' documentation
+    const opts = {
+        height: '500',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+    
+    const handleClink = (movie) => {
+        if (trailerUrl){
+            setUrl('')// closes the popup if its already open
+        }else {
+            movieTrailer(movie?.name || '')
+            .then((url) =>{
+                const urlParams = new URLSearchParams(new URL(url).search); // to get everything after the ? in the URL
+                setUrl(urlParams.get('v'));
+            })
+            .catch((error) => console.log(error));
+        }
+    }
     // console.log(movies)
     
     return (
@@ -25,11 +50,12 @@ const Row = ({title, fetchUrl, largePic}) => {
             <h2 className='title'>{title}</h2>
             <div className='row_posters'>
                 {movies.map((movie) => (
-                    <img key={movie.id} className={`row_poster ${largePic && 'row_poster_large'}`}
+                    <img key={movie.id} onClick={() => handleClink(movie)} className={`row_poster ${largePic && 'row_poster_large'}`}
                         src={`${BASE_URL}${largePic ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name}/>
                 ))}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
